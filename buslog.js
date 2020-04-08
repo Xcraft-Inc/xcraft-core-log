d57cd5667eff1f696879730e33bdb93cbdda501c
Mathieu Schroeter <mathieu@schroetersa.ch>
2020-04-08 15:10:22 +0200

    Cosmetic: fix formatting

--- buslog.js
+++ buslog.js
@@ -4,25 +4,25 @@ const xLog = require('.');
 
 const cmd = {};
 
-cmd.enable = function(msg, resp) {
+cmd.enable = function (msg, resp) {
   xLog.setEnable(true);
   resp.log.warn('buslog enabled, server performances can be impacted');
   resp.events.send(`buslog.enable.${msg.id}.finished`);
 };
 
-cmd.disable = function(msg, resp) {
+cmd.disable = function (msg, resp) {
   xLog.setEnable(false);
   resp.log.warn('buslog disabled, server returning in normal state');
   resp.events.send(`buslog.disable.${msg.id}.finished`);
 };
 
-cmd.modulenames = function(msg, resp) {
+cmd.modulenames = function (msg, resp) {
   const moduleNames = msg.data.modulenames || [];
   xLog.setModuleNames(moduleNames);
   resp.events.send(`buslog.modulenames.${msg.id}.finished`);
 };
 
-cmd.verbosity = function(msg, resp) {
+cmd.verbosity = function (msg, resp) {
   const level = msg.data.level;
   xLog.setGlobalVerbosity(level);
   resp.events.send(`buslog.verbosity.${msg.id}.finished`);
@@ -33,7 +33,7 @@ cmd.verbosity = function(msg, resp) {
  *
  * @returns {Object} The list and definitions of commands.
  */
-exports.xcraftCommands = function() {
+exports.xcraftCommands = function () {
   return {
     handlers: cmd,
     rc: {
